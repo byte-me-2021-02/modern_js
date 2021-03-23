5bcc37ab382bda167438e2356a028c63f9c702ba
brian <riceb53@gmail.com>
2021-03-22 22:21:03 -0400

    add babel

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 console.log('hello from js');
+console.log('hello from js');
+console.log('hello from js');
+console.log('hello from js');
+console.log('hello from js');
+console.log('hello from js');
+console.log('hello from js');
 var moment = require('moment'); // require
+var math = require('mathjs');
 // node.js
 
+console.log(math.sqrt(-4));
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+var name = "bob";
+var place = "mall";
+
+console.log(`${name} wants to go to the ${mall}`);
